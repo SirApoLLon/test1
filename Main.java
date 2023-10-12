d5e268f07b56c3e5274c77a52aff04b3f3f443cb
SirApoLLon <salo.kx@mail.ru>
2023-10-12 19:27:45 +0300

    init

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Программа для щифровки и д");
+        int n = scanner.nextInt();
+
+
+    }
+}
